03f94e77160f780575ccbe6e0abe1c702d1dbae1
Ben Zörb <ben@sommerlaune.com>
2021-12-05 23:53:25 +0100

    Adds day 1

---
+++ day-1/index.js
@@ -0,0 +1,33 @@
+const { run, ask, answer } = require('../lib/utils');
+
+const acc = (arr) => arr.reduce((last, cur) => last + cur, 0);
+
+run((input) => {
+  const [first, ...rows] = input.split(/[\r\n]/).map((num) => parseFloat(num));
+
+  const [, count, windows] = rows.reduce(
+    ([last, count, windows], current, index) => {
+      const currentWindow = [current];
+      for (let i = Math.max(index - 1, 0); i <= index; i++) {
+        windows[i] = [...(windows[i] || []), current];
+      }
+
+      return [current, current > last ? count + 1 : count, [...windows, currentWindow]];
+    },
+    [first, 0, [[first]]]
+  );
+
+  ask('How many measurements are larger than the previous measurement?');
+  answer(count);
+
+  ask('How many sums are larger than the previous sum?');
+  const [firstSum, ...sums] = windows.filter((window) => window.length === 3).map((window) => acc(window));
+  const [, countSum] = sums.reduce(
+    ([last, count], current) => {
+      return [current, current > last ? count + 1 : count];
+    },
+    [firstSum, 0]
+  );
+
+  answer(countSum);
+});
